6cfbb513a3acd304fdd0a8da1b2ce20131d9c03a
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-20 00:29:18 +0100

    create view for main menu screen

--- src/ts/screens/MainMenuScreen.ts
+++ src/ts/screens/MainMenuScreen.ts
@@ -1,1 +1,28 @@
-export default class MainMenuScreen {}
+export default class MainMenuScreen {
+  static createMainMenuScreen() {
+    const mainMenu = document.createElement("section") as HTMLDivElement;
+    mainMenu.classList.add("game");
+    mainMenu.innerHTML = `
+    <h1 class="text-[60px] mt-[50px]">🛡️ Duel Legends ⚔️</h1>
+        <button id="btn-start" class="btn glow">
+            <img
+            src="./assets/icons/icon_sword_shield.png"
+            alt="swords with shield icon"
+            />New Game
+        </button>
+    <footer>
+        <span class="font-ui text-[18px]">
+            Created by
+            <a
+              class="text-standard hover:text-hover duration-200"
+              target="_blank"
+              href="https://github.com/KamilKonopski"
+              >Kamil Konopski</a
+            >
+        </span>
+    </footer>
+    `;
+
+    return mainMenu;
+  }
+}
