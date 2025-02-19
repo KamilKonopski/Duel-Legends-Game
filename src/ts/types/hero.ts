af98fdf18e519e3e4ed68a868e05acdffba668fe
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-19 18:59:35 +0100

    remove to another folder

---
+++ src/ts/types/hero.ts
@@ -0,0 +1,25 @@
+export type HeroType = {
+  id: number;
+  name: string;
+  heroClass: string;
+  playstyle: string;
+  description: string;
+  abilities: {
+    id: number;
+    name: string;
+    description: string;
+    type: string;
+    value: number;
+    manaCost: number;
+  }[];
+  ultimate: {
+    name: string;
+    description: string;
+    type: string;
+    value: number;
+    manaCost: number;
+  }[];
+  healthPoints: number;
+  mana: number;
+  manaRegeneration: number;
+};
--- src/types/hero.ts
+++
@@ -1,9 +0,0 @@
-export type HeroType = {
-  id: number;
-  name: string;
-  heroClass: string;
-  playstyle: string;
-  description: string;
-  abilities: { id: number; name: string; description: string }[];
-  ultimate: { name: string; description: string }[];
-};
