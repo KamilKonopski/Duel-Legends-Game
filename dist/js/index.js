6bde62cebe11a5fba80c029df3f1bd5f3f3d695b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-20 00:59:23 +0100

    test for view screens

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,12 @@
-const hello = "Hello";
+import GameOverScreen from "./screens/GameOverScreen.js";
+import MainMenuScreen from "./screens/MainMenuScreen.js";
+const gameContainer = document.querySelector(".game-container");
+function render() {
+    gameContainer.appendChild(MainMenuScreen.createMainMenuScreen());
+}
+render();
+const startButton = document.getElementById("btn-start");
+startButton.addEventListener("click", () => {
+    gameContainer.innerHTML = "";
+    gameContainer.appendChild(GameOverScreen.createGameOverScreen(20, "win"));
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,20 @@
-const hello = "Hello";
+import GameOverScreen from "./screens/GameOverScreen.js";
+import MainMenuScreen from "./screens/MainMenuScreen.js";
+
+const gameContainer = document.querySelector(
+  ".game-container"
+) as HTMLDivElement;
+
+function render() {
+  gameContainer.appendChild(MainMenuScreen.createMainMenuScreen());
+}
+
+render();
+
+const startButton = document.getElementById("btn-start") as HTMLButtonElement;
+
+startButton.addEventListener("click", () => {
+  gameContainer.innerHTML = "";
+
+  gameContainer.appendChild(GameOverScreen.createGameOverScreen(20, "win"));
+});
