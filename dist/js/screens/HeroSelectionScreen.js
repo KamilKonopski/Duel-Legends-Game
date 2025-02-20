8fc1a2eca176c98cdd58015846664fc9159f2feb
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-21 00:05:38 +0100

    add function to create content in Html

--- dist/js/screens/HeroSelectionScreen.js
+++ dist/js/screens/HeroSelectionScreen.js
@@ -1,2 +1,47 @@
+import { heroesData } from "../../../data/heroes.js";
 export default class HeroSelectionScreen {
+    static createHeroSelectionScreen() {
+        const heroSelection = document.createElement("section");
+        heroSelection.classList.add("game");
+        heroSelection.innerHTML = `
+    <h2 class="mt-[100px] text-4xl font-ui">Choose Your Hero!</h2>
+    `;
+        heroSelection.appendChild(this.createHeroChoiceContainer());
+        heroSelection.appendChild(this.createPlayButton());
+        return heroSelection;
+    }
+    static createHeroChoiceContainer() {
+        const heroChoiceContainer = document.createElement("div");
+        const heroes = heroesData;
+        for (let i = 1; i <= heroes.length; i++) {
+            heroChoiceContainer.appendChild(this.createHeroChoice());
+        }
+        return heroChoiceContainer;
+    }
+    static createHeroChoice() {
+        const heroChoice = document.createElement("div");
+        heroChoice.innerHTML = `
+    <div>
+      <span>Sir Leon</span>
+      <span>Knight 🛡⚔</span>
+    </div>
+    <div>
+    <span>Hp: 1200</span>
+    <span>Mana: 500</span>
+    </div>
+    <div>
+    <span>Attack: Sword Strike ⚔</span>
+    <span>Defence: Shield Block 🛡</span>
+    <span>Skill: Critical Slash 💥</span>
+    <span>ULT: Divine Protection ✨</span>
+    </div>
+    `;
+        return heroChoice;
+    }
+    static createPlayButton() {
+        const playButton = document.createElement("button");
+        playButton.classList.add("mb-32", "btn", "glow");
+        playButton.innerText = "PLAY";
+        return playButton;
+    }
 }
--- src/ts/screens/HeroSelectionScreen.ts
+++ src/ts/screens/HeroSelectionScreen.ts
@@ -1,1 +1,60 @@
-export default class HeroSelectionScreen {}
+import { heroesData } from "../../../data/heroes.js";
+
+import { HeroType } from "../types/hero";
+
+export default class HeroSelectionScreen {
+  static createHeroSelectionScreen() {
+    const heroSelection = document.createElement("section") as HTMLDivElement;
+    heroSelection.classList.add("game");
+    heroSelection.innerHTML = `
+    <h2 class="mt-[100px] text-4xl font-ui">Choose Your Hero!</h2>
+    `;
+
+    heroSelection.appendChild(this.createHeroChoiceContainer());
+    heroSelection.appendChild(this.createPlayButton());
+
+    return heroSelection;
+  }
+
+  private static createHeroChoiceContainer() {
+    const heroChoiceContainer = document.createElement("div") as HTMLDivElement;
+
+    const heroes: HeroType[] = heroesData;
+
+    for (let i = 1; i <= heroes.length; i++) {
+      heroChoiceContainer.appendChild(this.createHeroChoice());
+    }
+
+    return heroChoiceContainer;
+  }
+
+  private static createHeroChoice() {
+    const heroChoice = document.createElement("div") as HTMLDivElement;
+    heroChoice.innerHTML = `
+    <div>
+      <span>Sir Leon</span>
+      <span>Knight 🛡⚔</span>
+    </div>
+    <div>
+    <span>Hp: 1200</span>
+    <span>Mana: 500</span>
+    </div>
+    <div>
+    <span>Attack: Sword Strike ⚔</span>
+    <span>Defence: Shield Block 🛡</span>
+    <span>Skill: Critical Slash 💥</span>
+    <span>ULT: Divine Protection ✨</span>
+    </div>
+    `;
+
+    return heroChoice;
+  }
+
+  private static createPlayButton() {
+    const playButton = document.createElement("button") as HTMLButtonElement;
+    playButton.classList.add("mb-32", "btn", "glow");
+    playButton.innerText = "PLAY";
+
+    return playButton;
+  }
+}
