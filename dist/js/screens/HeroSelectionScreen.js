b9241e36699e43a54f3b9074352bebde7f2e1f25
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-24 16:43:37 +0100

    remove single hero function

--- dist/js/screens/HeroSelectionScreen.js
+++ dist/js/screens/HeroSelectionScreen.js
@@ -8,6 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import { fetchHeroesData } from "../services/fetchHeroesData.js";
+import Hero from "../logic/Hero.js";
 export default class HeroSelectionScreen {
     static createHeroSelectionScreen() {
         return __awaiter(this, void 0, void 0, function* () {
@@ -16,7 +17,7 @@ export default class HeroSelectionScreen {
             heroSelection.classList.add("game");
             heroSelection.dataset.screen = "2";
             heroSelection.innerHTML = `
-    <h2 class="mt-[100px] text-4xl font-ui">Choose Your Hero!</h2>
+    <h2 class="mt-[100px] text-4xl">Choose Your Hero!</h2>
     `;
             heroSelection.appendChild(this.createHeroChoiceContainer(heroes));
             heroSelection.appendChild(this.createPlayButton());
@@ -26,30 +27,8 @@ export default class HeroSelectionScreen {
     static createHeroChoiceContainer(heroes) {
         const heroChoiceContainer = document.createElement("div");
         heroChoiceContainer.classList.add("hero-container");
-        heroes.forEach((hero) => heroChoiceContainer.appendChild(this.createHeroChoice(hero)));
+        heroes.forEach((hero) => heroChoiceContainer.appendChild(Hero.createHero(hero)));
         return heroChoiceContainer;
-    }
-    static createHeroChoice(hero) {
-        const heroChoice = document.createElement("div");
-        heroChoice.classList.add("hero", "glow", "bordered-letter");
-        heroChoice.style.backgroundImage = `url(${hero.heroImage})`;
-        heroChoice.innerHTML = `
-    <div class="mb-1.5 text-[14px] flex flex-col items-center border-y border-border w-full">
-      <span class="uppercase font-bold">${hero.name}</span>
-      <span>${hero.heroClass}</span>
-    </div>
-    <div class="flex w-full justify-between">
-    <span class="border-y border-border">Hp: ${hero.healthPoints}</span>
-    <span class="border-y border-border">Mana: ${hero.mana}</span>
-    </div>
-    <div class="flex flex-col items-center w-full mt-3 mb-2">
-    <span class="text-attack">Attack: ${hero.abilities[0].name}</span>
-    <span class="text-defense">Defence: ${hero.abilities[1].name}</span>
-    <span class="text-ability">Skill: ${hero.abilities[2].name}</span>
-    <span class="text-standard">ULT: ${hero.ultimate.name}</span>
-    </div>
-    `;
-        return heroChoice;
     }
     static createPlayButton() {
         const playButton = document.createElement("button");
--- src/ts/screens/HeroSelectionScreen.ts
+++ src/ts/screens/HeroSelectionScreen.ts
@@ -1,5 +1,8 @@
-import { fetchHeroesData } from "../services/fetchHeroesData.js";
 import { HeroType } from "../types/hero";
+
+import { fetchHeroesData } from "../services/fetchHeroesData.js";
+
+import Hero from "../logic/Hero.js";
 
 export default class HeroSelectionScreen {
   static async createHeroSelectionScreen() {
@@ -9,7 +12,7 @@ export default class HeroSelectionScreen {
     heroSelection.classList.add("game");
     heroSelection.dataset.screen = "2";
     heroSelection.innerHTML = `
-    <h2 class="mt-[100px] text-4xl font-ui">Choose Your Hero!</h2>
+    <h2 class="mt-[100px] text-4xl">Choose Your Hero!</h2>
     `;
 
     heroSelection.appendChild(this.createHeroChoiceContainer(heroes));
@@ -23,35 +26,10 @@ export default class HeroSelectionScreen {
     heroChoiceContainer.classList.add("hero-container");
 
     heroes.forEach((hero) =>
-      heroChoiceContainer.appendChild(this.createHeroChoice(hero))
+      heroChoiceContainer.appendChild(Hero.createHero(hero))
     );
 
     return heroChoiceContainer;
-  }
-
-  private static createHeroChoice(hero: HeroType) {
-    const heroChoice = document.createElement("div") as HTMLDivElement;
-    heroChoice.classList.add("hero", "glow", "bordered-letter");
-    heroChoice.style.backgroundImage = `url(${hero.heroImage})`;
-
-    heroChoice.innerHTML = `
-    <div class="mb-1.5 text-[14px] flex flex-col items-center border-y border-border w-full">
-      <span class="uppercase font-bold">${hero.name}</span>
-      <span>${hero.heroClass}</span>
-    </div>
-    <div class="flex w-full justify-between">
-    <span class="border-y border-border">Hp: ${hero.healthPoints}</span>
-    <span class="border-y border-border">Mana: ${hero.mana}</span>
-    </div>
-    <div class="flex flex-col items-center w-full mt-3 mb-2">
-    <span class="text-attack">Attack: ${hero.abilities[0].name}</span>
-    <span class="text-defense">Defence: ${hero.abilities[1].name}</span>
-    <span class="text-ability">Skill: ${hero.abilities[2].name}</span>
-    <span class="text-standard">ULT: ${hero.ultimate.name}</span>
-    </div>
-    `;
-
-    return heroChoice;
   }
 
   private static createPlayButton() {
