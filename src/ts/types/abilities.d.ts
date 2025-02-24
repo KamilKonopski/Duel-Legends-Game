3bc96cc87e453962e454f948bd0bdd1f392ad185
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-24 16:42:00 +0100

    create Abilities and Ultimate Types

---
+++ src/ts/types/abilities.d.ts
@@ -0,0 +1,16 @@
+export type AbilitiesType = {
+  id: number;
+  name: string;
+  description: string;
+  type: string;
+  value: number;
+  manaCost: number;
+}[];
+
+export type UltimateType = {
+  name: string;
+  description: string;
+  type: string;
+  value: number;
+  manaCost: number;
+};
