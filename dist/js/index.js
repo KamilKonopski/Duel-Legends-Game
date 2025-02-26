2b8a9ac09c2906ee5f43360dec034fb842ed8012
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-02-27 00:49:22 +0100

    add render function to quick change screen for develop

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,31 @@
-import GameOverScreen from "./screens/GameOverScreen.js";
-import MainMenuScreen from "./screens/MainMenuScreen.js";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// import MainMenuScreen from "./screens/MainMenuScreen.js";
+// import HeroSelectionScreen from "./screens/HeroSelectionScreen.js";
+import BattleScreen from "./screens/BattleScreen.js";
+// import GameOverScreen from "./screens/GameOverScreen.js";
 const gameContainer = document.querySelector(".game-container");
+// function render() {
+//   gameContainer.appendChild(MainMenuScreen.createMainMenuScreen());
+// }
+// async function render() {
+//   gameContainer.appendChild(
+//     await HeroSelectionScreen.createHeroSelectionScreen()
+//   );
+// }
 function render() {
-    gameContainer.appendChild(MainMenuScreen.createMainMenuScreen());
+    return __awaiter(this, void 0, void 0, function* () {
+        gameContainer.appendChild(yield BattleScreen.createBattleScreen());
+    });
 }
+// function render() {
+//   gameContainer.appendChild(GameOverScreen.createGameOverScreen(20, "win"));
+// }
 render();
-const startButton = document.getElementById("btn-start");
-startButton.addEventListener("click", () => {
-    gameContainer.innerHTML = "";
-    gameContainer.appendChild(GameOverScreen.createGameOverScreen(20, "win"));
-});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,20 +1,28 @@
-import GameOverScreen from "./screens/GameOverScreen.js";
-import MainMenuScreen from "./screens/MainMenuScreen.js";
+// import MainMenuScreen from "./screens/MainMenuScreen.js";
+// import HeroSelectionScreen from "./screens/HeroSelectionScreen.js";
+import BattleScreen from "./screens/BattleScreen.js";
+// import GameOverScreen from "./screens/GameOverScreen.js";
 
 const gameContainer = document.querySelector(
   ".game-container"
 ) as HTMLDivElement;
 
-function render() {
-  gameContainer.appendChild(MainMenuScreen.createMainMenuScreen());
+// function render() {
+//   gameContainer.appendChild(MainMenuScreen.createMainMenuScreen());
+// }
+
+// async function render() {
+//   gameContainer.appendChild(
+//     await HeroSelectionScreen.createHeroSelectionScreen()
+//   );
+// }
+
+async function render() {
+  gameContainer.appendChild(await BattleScreen.createBattleScreen());
 }
 
+// function render() {
+//   gameContainer.appendChild(GameOverScreen.createGameOverScreen(20, "win"));
+// }
+
 render();
-
-const startButton = document.getElementById("btn-start") as HTMLButtonElement;
-
-startButton.addEventListener("click", () => {
-  gameContainer.innerHTML = "";
-
-  gameContainer.appendChild(GameOverScreen.createGameOverScreen(20, "win"));
-});
